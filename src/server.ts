da69db13676547b6de0b128568a73656744a2671
David <learnitmyway@users.noreply.github.com>
2020-03-30 16:10:50 +0200

    npx prettier@2.0.2 . --write

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express from 'express'
+import bodyParser from 'body-parser'
+import { filterImageFromURL, deleteLocalFiles } from './util/util'
 
-(async () => {
-
+;(async () => {
   // Init the Express application
-  const app = express();
+  const app = express()
 
   // Set the network port
-  const port = process.env.PORT || 8082;
-  
+  const port = process.env.PORT || 8082
+
   // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+  app.use(bodyParser.json())
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
@@ -30,17 +29,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get('/', async (req, res) => {
+    res.send('try GET /filteredimage?image_url={{}}')
+  })
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
-})();
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`)
+    console.log(`press CTRL+C to stop server`)
+  })
+})()
